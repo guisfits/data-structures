4e981b0f4938049357b289b8d84b9ad0f9943b01
Guilherme Camargo <guisfits@hotmail.com>
2020-04-25 20:24:42 -0300

    finishing weighted graphs

--- DataStructures/WeightedGraph/WeightedGraph.cs
+++ DataStructures/WeightedGraph/WeightedGraph.cs
@@ -8,12 +8,10 @@ namespace DataStructures
         #region Fields / Constructor
 
         private Dictionary<T, Node> _nodes;
-        private Dictionary<Node, List<Edge>> _adjacencyList;
 
         public WeightedGraph()
         {
             _nodes = new Dictionary<T, Node>();
-            _adjacencyList = new Dictionary<Node, List<Edge>>();
         }
 
         #endregion
@@ -31,48 +29,86 @@ namespace DataStructures
             if (fromNode == null || toNode == null)
                 return;
 
-            _adjacencyList
-                .GetValueOrAdd(fromNode)
-                .Add(new Edge(fromNode, toNode, weight));
-
-            _adjacencyList
-                .GetValueOrAdd(toNode)
-                .Add(new Edge(toNode, fromNode, weight));
+            fromNode.AddEdge(toNode, weight);
+            toNode.AddEdge(fromNode, weight);
         }
 
         public Dictionary<T, IEnumerable<T>> GetAll()
         {
             var result = new Dictionary<T, IEnumerable<T>>();
             foreach (var node in _nodes.Values)
-                result.Add(node.Key, _adjacencyList.GetValue(node)?.Select(x => x.To.Key));
+                result.Add(node.Key, node.GetEdges().Select(x => x.To.Key));
 
             return result;
+        }
+
+        public bool HasCycle(T Key)
+        {
+            var visited = new HashSet<Node>();
+            foreach (var node in _nodes.Values)
+            {
+                if (!visited.Contains(node) && HasCycle(node, null, visited))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool HasCycle(Node node, Node parent, ISet<Node> visited)
+        {
+            visited.Add(node);
+
+            foreach (var edge in node.GetEdges())
+            {
+                if (edge.To == parent)
+                    continue;
+
+                if (visited.Contains(edge.To) || HasCycle(edge.To, node, visited))
+                    return true;
+            }
+
+            return false;
         }
 
         #region Classes
 
         class Node
         {
+            List<Edge> _edges;
             public Node(T key)
             {
                 Key = key;
+                _edges = new List<Edge>();
             }
 
             public T Key { get; }
+
+            public void AddEdge(Node to, int weight)
+            {
+                this._edges.Add(new Edge(to, weight));
+            }
+
+            public Edge[] GetEdges()
+            {
+                return _edges.ToArray();
+            }
         }
 
-        class Edge
+        class Edge : IComparer<Edge>
         {
-            public Edge(Node from, Node to, int weight)
+            public Edge(Node to, int weight)
             {
-                From = from;
                 To = to;
                 Weight = weight;
             }
 
-            public Node From { get; }
             public Node To { get; }
             public int Weight { get; }
+
+            public int Compare(Edge x, Edge y)
+            {
+                return x.Weight - y.Weight;
+            }
         }
 
         #endregion
